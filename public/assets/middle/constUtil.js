cbda32da8fb6446b54f3cab7ecb64327b0d3d874
zhangxiangnan <gaulle@126.com>
2015-12-01 14:59:06 +0800

    changes log

--- public/assets/middle/constUtil.js
+++ public/assets/middle/constUtil.js
@@ -1,12 +1,14 @@
 /**
  * 后台管理模块存放常量的js
  */
+var host = window.location.host;
 // 后台ajax基准地址
-var ajaxRootURL = "http://127.0.0.1:11002/xinlan";
+var ajaxRootURL = "http://"+host+"/xinlan";
 // 图片基准地址
 var imgRootURL = "http://127.0.0.1:11001/images";
 // 后台上传图片基准地址
-var fileUploadRootURL = "http://127.0.0.1:11002/upload";
+var fileUploadRootURL = "http://"+host+"/upload";
+
 // 投票后台ajax地址
 var ajaxVoteURL = ajaxRootURL + "/votes";
 // 投票后台上传图片地址
--- public/votes/js/constUtilVotes.js
+++ public/votes/js/constUtilVotes.js
@@ -1,7 +1,8 @@
 /**
  * 存放投票模块常量的js
  */
+var host = window.location.host;
 // 后台ajax地址
-var ajaxURL = "http://127.0.0.1:11002/xinlan/votes";
+var ajaxURL = "http://"+host+"/xinlan/votes";
 // 图片基准地址
 var imgURL = "http://127.0.0.1:11001/images/votes";
